8f9b5a48b4f82e4b8da33d06688cdb7007ad9cf5
Marlon Savian <marlon.savian@gmail.com>
2021-04-05 10:08:21 -0300

    add burger ingredient

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AuxHoc from '../../hoc/AuxHoc'
+import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient'
 import './Layout.css'
 
 const layout = (props)=>(
     <AuxHoc>
     <div>Toolbar,SideDrawer,Backdrop</div>
+    
     <main className="Content"   >
         {props.children}
     </main>
