37d054a7e94fdd77db0f68365059cfa38f99e113
Marlon Savian <marlon.savian@gmail.com>
2021-04-23 15:11:24 -0300

    add modal error

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,27 @@
+import classes from "./ErrorModal.module.css";
+import Card from "./Card";
+import Button from "./Button";
+
+
+const ErrorModal = (props) => {
+
+  return (      
+    <div>
+      <div className={classes.backdrop} onClick={props.closeModal}>
+        <Card className={classes.modal}>
+          <header className={classes.header}>
+            <h2>{props.title}</h2>
+          </header>
+          <div className={classes.content}>
+            <p>{props.content}</p>
+          </div>
+          <footer className={classes.actions}>
+            <Button onClick={props.closeModal}>Okay</Button>
+          </footer>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorModal;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,13 @@
 import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 import { useState } from "react";
 
 const AddUser = (props) => {
   const [userName, setUserName] = useState("");
   const [userAge, setUserAge] = useState("");
+  const [error, setError] = useState();
 
   const onChangeName = (event) => {
     setUserName(event.target.value);
@@ -14,46 +16,65 @@ const AddUser = (props) => {
     setUserAge(event.target.value);
   };
 
-  const onSubmit = (event) => {      
+  const onSubmit = (event) => {
     event.preventDefault();
-    
-    if (userName.trim().length <= 0 || userAge.trim().length <= 0) {
+
+    if (userName.trim().length <= 0) {
+      setError({
+        title: "Username Invalid!",
+        content: "Username must be filled!",
+      });
       return;
     }
+
+    if (userAge.trim().length <= 0) {
+      setError({ title: "Age Invalid!", content: "Age must be filled!" });
+      return;
+    }
+
     if (+userAge < 1) {
+      setError({
+        title: "Age Invalid!",
+        content: "Age must be higher than 0!",
+      });
       return;
     }
 
-    const newUser = {name:userName, age:userAge}
+    const newUser = { name: userName, age: userAge };
 
-    props.addUserHandler(newUser)
+    props.addUserHandler(newUser);
 
     setUserName("");
     setUserAge("");
   };
 
+  const closeModalError =()=>{
+    setError(null)
+  }
   return (
-    <Card className={classes.input}>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={userName}
-          onChange={onChangeName}
-        />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          min="1"
-          max="150"
-          onChange={onChangeAge}
-          value={userAge}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <>
+      {error && <ErrorModal title={error.title} content={error.content} closeModal={closeModalError}/>}
+      <Card className={classes.input}>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={userName}
+            onChange={onChangeName}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            
+            onChange={onChangeAge}
+            value={userAge}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </>
   );
 };
 
