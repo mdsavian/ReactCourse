659e0f57a2e7b378f39946bb89343fa88dfa5d85
Marlon Savian <marlon.savian@gmail.com>
2021-04-27 21:29:03 -0300

    add bump

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,18 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../../Cart/CartIcon";
 import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
   const ctx = useContext(CartContext);
-
-  const numberOfCartItems = ctx.items.reduce((prevValue, currentValue) => {
+  const { items } = ctx;
+  const numberOfCartItems = items.reduce((prevValue, currentValue) => {
     return prevValue + currentValue.amount;
   }, 0);
 
+  useEffect(() => {
+    if (items.length === 0) return;
+
+    setBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const btnClasses = `${classes.button}  ${btnHighlighted ? classes.bump : ""}`;
+
   return (
     <>
-      <button className={classes.button} onClick={props.openCart}>
+      <button className={btnClasses} onClick={props.openCart}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
