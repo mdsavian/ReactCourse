70033657195e59d351c8150cdcafe9b9dd21e0ab
Marlon Savian <marlon.savian@gmail.com>
2021-05-11 09:13:41 -0300

    add redux store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,29 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const toggleCounterHandler = (type, value = 0) => {
+    dispatch({ type: type, amount: value });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={() => toggleCounterHandler("increment")}>
+          Incremet
+        </button>
+        <button onClick={() => toggleCounterHandler("increase", 5)}>
+          Increase By 5
+        </button>
+        <button onClick={() => toggleCounterHandler("decrement")}>
+          Decrement
+        </button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux";
+
+const initialState = { counter: 0 };
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return { counter: state.counter + 1 };
+  }
+
+  if (action.type === "increase") {
+    return { counter: state.counter + action.amount };
+  }
+
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1 };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
